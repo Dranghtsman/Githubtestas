fb16dcad4be99821c3226d172ccfbb514bdd73c5
Dranghtsman <71728984+Dranghtsman@users.noreply.github.com>
2020-09-30 11:00:59 +0300

    Add files via upload

---
+++ PrintXNewCh.java
@@ -0,0 +1,11 @@
+public class PrintXNewCh {
+    public static void main(String[] args) {
+
+        String x = "\n    " + "*" + "ccccc" + "*" + "\n     " + "*" + "ccc" + "*" + "\n      " + "*" + " " + "*" +
+                "\n     " + "*" + "ccc" + "*" + "\n    " + "*" + "ccccc" + "*" + "\n   " + "*" + "ccccccc" + "*";
+
+        System.out.println(x.replace('c', ' '));
+
+    }
+}
+
